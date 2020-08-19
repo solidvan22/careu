9e74892e58d8fcc89bc9bfe5f83116cc4e7046fa
Megan Montserrat <megan@careu-server.us-east1-b.c.long-ratio-284417.internal>
2020-08-19 15:07:19 +0000

    database init

--- database/mongo_init.js
+++ database/mongo_init.js
@@ -1,12 +1,11 @@
 var MongoClient = require('mongodb').MongoClient;
-var config      = require('../config/env.config')
-let {mongodbServer, databaseName} = config.get();
+let mongodbServer = "application:Chacha123@mongodb://35.231.29.183:27017"
+let databaseName = "careu-db"
 var dbInstance;
 console.log('mongodbServer' , mongodbServer); 
 function connect(){
     return new Promise((resolve,reject)=>{
         MongoClient.connect(mongodbServer+ databaseName,function (err, client){
-            //MongoClient.connect('mongodb://localhost:27017/WebHelpPruebas',function (err, db){
                 if(err) {
                     console.log("Error al intentar conectar a la BD: " + err);
                     reject(err);
