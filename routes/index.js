7fe37bb0cbf79017147873fddf94114b9d307414
Megan Montserrat <meganretana101@gmail.com>
2020-08-29 14:40:27 +0000

    clean public folder

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,40 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 router.get('/home', function (req, res, next) {
-  res.render('feed', { title: 'Express' });
+  res.render('home', { title: 'Express' });
+
 });
+
+router.get('/self-care', function (req, res, next) {
+  res.render('feed', { title: 'Self Care' });
+});
+
+router.get('/meditation', function (req, res, next) {
+  res.render('feed', { title: 'Meditation' });
+});
+
+router.get('/movement', function (req, res, next) {
+  res.render('feed', { title: 'Movement' });
+});
+
+router.get('/mainresources', function (req, res, next) {
+  res.render('feed', { title: 'Maine Resources' });
+});
+
+router.get('/virtuallearningtips', function (req, res, next) {
+  res.render('feed', { title: 'Virtual Learning Tips' });
+});
+
+router.get('/profile', function (req, res, next) {
+  res.render('profile', { title: 'Profile' });
+});
+
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Login',layout:false });
+});
+
+
 
 module.exports = router;
